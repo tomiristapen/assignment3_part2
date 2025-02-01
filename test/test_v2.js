73603eba7499c0ef82c04f45fd169faae5ec9837
tomiristapen <tomiristn@gmail.com>
2025-02-01 17:25:23 +0600

    Initial commit for assignment3_part2

--- test/test_v2.js
+++ test/test_v2.js
@@ -1,25 +1,27 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("ERC20_smart_contract", function () {
-  async function deployTokenFixture() {
-    const Token = await ethers.getContractFactory("ERC20_smart_contract");
+describe("AITU_SE_2314_16Token2", function () {
+  async function deployTokenFixture(initialSupply) {
+    const Token = await ethers.getContractFactory("AITU_SE_2314_16Token2");
     const [owner, addr1, addr2] = await ethers.getSigners();
 
-    const token = await Token.deploy(1000); // Pass initial supply to the constructor
+    const token = await Token.deploy(initialSupply); // Pass initial supply to the constructor
     await token.deployed();
 
     return { token, owner, addr1, addr2 };
   }
 
   it("Deployment should assign total supply to the owner", async function () {
-    const { token, owner } = await deployTokenFixture();
+    const initialSupply = 1000;
+    const { token, owner } = await deployTokenFixture(initialSupply);
     const ownerBalance = await token.balanceOf(owner.address);
     expect(await token.totalSupply()).to.equal(ownerBalance);
   });
 
   it("Should transfer tokens between accounts", async function () {
-    const { token, owner, addr1, addr2 } = await deployTokenFixture();
+    const initialSupply = 1000;
+    const { token, owner, addr1, addr2 } = await deployTokenFixture(initialSupply);
     await token.transfer(addr1.address, 50);
     expect(await token.balanceOf(addr1.address)).to.equal(50);
 
@@ -28,21 +30,24 @@ describe("ERC20_smart_contract", function () {
   });
 
   it("Should emit Transfer events", async function () {
-    const { token, owner, addr1 } = await deployTokenFixture();
+    const initialSupply = 1000;
+    const { token, owner, addr1 } = await deployTokenFixture(initialSupply);
     await expect(token.transfer(addr1.address, 50))
       .to.emit(token, "Transfer")
       .withArgs(owner.address, addr1.address, 50);
   });
 
   it("Should retrieve last sender and receiver addresses", async function () {
-    const { token, owner, addr1 } = await deployTokenFixture();
+    const initialSupply = 1000;
+    const { token, owner, addr1 } = await deployTokenFixture(initialSupply);
     await token.transfer(addr1.address, 50);
     expect(await token.getLastSenderAddress()).to.equal(owner.address);
     expect(await token.getLastReceiverAddress()).to.equal(addr1.address);
   });
 
   it("Should retrieve last transaction timestamp", async function () {
-    const { token, owner, addr1 } = await deployTokenFixture();
+    const initialSupply = 1000;
+    const { token, owner, addr1 } = await deployTokenFixture(initialSupply);
     await token.transfer(addr1.address, 50);
     const block = await ethers.provider.getBlock("latest");
     expect(await token.lastTimestamp()).to.equal(block.timestamp);
